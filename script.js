bd5f68a5c20fed29e1a6ccd31d28cb51fc69f11c
webmarket-store <67576001+webmarket-store@users.noreply.github.com>
2020-08-23 18:42:25 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,86 @@
 var flag = 1;
 
-function sidebar(){
-    if(flag==1){
-        document.getElementById("sidebar").style.display="block";
-        flag=0;
-    }
-   
-    else{
-    document.getElementById("sidebar").style.display="none";  
+function show() {
+  if(flag==1){
+    let menu = document.getElementById("sidebar").style="left:0px;transition: 0.3s;";
+    flag=0;
+} else{
+    document.getElementById("sidebar").style="left:-500px;transition: 0.5s;";  
     flag=1;
 
-}
+} 
+    }
+ 
+ 
+
+    function morstory() {
+document.getElementById("bigcont").style.display="none";
+    document.getElementById("biginfo").style.display="block";
+       document.getElementById("closeinfo").style.display="block";
+   
+    }
+        
+
+function hideinfo(){
+ document.getElementById("bigcont").style.display="block";
+  document.getElementById("biginfo").style.display="none";
+ document.getElementById("closeinfo").style.display="none";
+} 
+let rise = 1;
+
+
+function likerise(){
+
+    if(rise===1){
+      let cyan =  document.getElementById('like-btn').style="color:cyan;";
+        rise=0;
+        let likenum=document.getElementById('like-txt').innerHTML=""+1 ;
+        document.getElementById('like-txt').style="color:white;"
+
+        document.getElementById('vid-info').style="border-bottom:1px solid cyan;";
+    }else{
+      let white =  document.getElementById('like-btn').style="color:white;";
+        rise=1;
+        let disnum=document.getElementById('like-txt').innerHTML="1"-1;
+        document.getElementById('like-txt').style="color:rgb(138, 136, 136);"
+        document.getElementById('vid-info').style="border-bottom:1px solid white;";
+    }
+
+
+}       
+
+function comeshow(){
+  if(rise==1){
+  document.getElementById('comentshow').style="height:600px;transition: 0.2s;";
+  document.getElementById('showcome').innerHTML="hide comments";
+
+  rise=0;
+  }else{
+    document.getElementById('comentshow').style="height:40px;transition: 0.2s;";
+    document.getElementById('showcome').innerHTML="show comments";
+ 
+    rise=1;
+  }
 
 }
-function tranc(){
-
-if(ul==1){
-    document.getElementById("sidebar").style.transition="10s ease all"
-ul=0;
-    
+var map = 1;
+function setseting(){
+  if(map===1){
+document.getElementById('them-settings-top').style=" transform: rotate(40deg);"
+document.getElementById('mainsetting').style.display="block";
+map=0;
+  }else{
+    document.getElementById('them-settings-top').style=" transform: rotate(0deg);"
+    document.getElementById('mainsetting').style.display="none";
+    map=1;
+  }
 }
-else{
-    document.getElementById("sidebar").style.transition="10s ease all"
-    ul=1;
-}
-}
 
 
+
+ 
+
+
+   
+  
 
